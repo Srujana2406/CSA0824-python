5649db2dda92b7a7c42950cd656dbeedf00ec1a0
Srujana2406 <129171029+Srujana2406@users.noreply.github.com>
2024-03-21 09:32:52 +0530

    Add files via upload

---
+++ difference between 2 dtaes.py
@@ -0,0 +1,5 @@
+from datetime import date
+first_date=date(2024,10,24)
+second_date=date(2024,11,1)
+diff=second_date-first_date
+print(f"number of days betweem {first_date} and {second_date}:{diff.days}")
